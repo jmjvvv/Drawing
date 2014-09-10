c87b19ce8e793b6d80e78bd8c32b2ae79259fde8
jmjvvv <jmjvvv@gmail.com>
2014-09-10 17:02:09 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(250, 250);
   background(0);
   stroke(255);
   noFill();
-  line(10, 10, 90, 10);
+  line(101, 28, 107, 28);
+  line(98, 29, 109, 29);
+  line(96, 30, 111, 30);
+  line(94, 31, 112, 31);
     rect(20, 20, 80, 50);
     ellipse(50, 50, 50, 50);
 }
